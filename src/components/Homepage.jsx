4fbb5e8e8d7c0fb10ac67c22892815d24ffd6346
Jetnik Syla <sjetnik@gmail.com>
2024-05-07 23:25:23 -0400

    add toggle functionality for additional social icons on homepage
    Added a toggle state to the Homepage component to control the visibility of social icons beyond GitHub, Heroku & LinkedIn. Now, users can click the 'Show More...' button to display additional social media icons, improving the user interface and customization of the profile section.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,27 @@
-import { BsFacebook, BsTwitter, BsInstagram, BsSpotify } from "react-icons/bs";
+import {
+  BsFacebook,
+  BsTwitter,
+  BsInstagram,
+  BsSpotify,
+  BsGithub,
+  BsLinkedin,
+} from "react-icons/bs";
+import { SiHeroku } from "react-icons/si";
 import AvatarPic from "../assets/avatar.png";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 const Homepage = () => {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <>
       <section className="font-roboto flex flex-col flex-wrap lg:flex-row justify-center items-center from-blue-200 to-indigo-100 text-gray-800 p-5 lg:p-20 min-h-screen space-y-10 lg:space-x-10">
         <div className="flex flex-col items-center w-full lg:w-1/2 space-y-5">
-          <p className="text-3xl lg:text-4xl font-semibold text-indigo-700">
+          <p className="text-2xl lg:text-4xl font-semibold text-indigo-700">
             👋 I'm
           </p>
-          <h1 className="text-7xl lg:text-6xl font-bold text-gray-800">
+          <h1 className="text-2xl lg:text-5xl font-bold text-gray-800">
             Jetnik Syla
           </h1>
           <hr className="border-t-4 border-indigo-300 w-full" />
@@ -36,29 +47,45 @@ const Homepage = () => {
             Elevating digital experiences with cutting-edge solutions and a
             creative approach.
           </p>
-          <Link
-            to="/about"
+          <button
+            onClick={() => setShowMore(!showMore)}
             className="px-10 py-3 rounded-lg text-lg font-medium shadow-md bg-indigo-600 text-white hover:bg-indigo-700 transition-colors duration-300"
           >
             Show More...
-          </Link>
+          </button>
           <div className="flex space-x-4">
-            <BsFacebook
+            <BsGithub
               size={35}
-              className="text-blue-600 hover:text-blue-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
+              className="text-gray-600 hover:text-black cursor-pointer transition-all duration-300 transform hover:scale-110"
             />
-            <BsTwitter
+            <BsLinkedin
               size={35}
-              className="text-blue-400 hover:text-blue-600 cursor-pointer transition-all duration-300 transform hover:scale-110"
+              className="text-blue-700 hover:text-blue-900 cursor-pointer transition-all duration-300 transform hover:scale-110"
             />
-            <BsInstagram
+            <SiHeroku
               size={35}
-              className="text-pink-600 hover:text-pink-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
+              className="text-purple-600 hover:text-purple-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
             />
-            <BsSpotify
-              size={35}
-              className="text-green-600 hover:text-green-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
-            />
+            {showMore && (
+              <>
+                <BsFacebook
+                  size={35}
+                  className="text-blue-600 hover:text-blue-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
+                />
+                <BsInstagram
+                  size={35}
+                  className="text-pink-600 hover:text-pink-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
+                />
+                <BsSpotify
+                  size={35}
+                  className="text-green-600 hover:text-green-800 cursor-pointer transition-all duration-300 transform hover:scale-110"
+                />
+                <BsTwitter
+                  size={35}
+                  className="text-blue-400 hover:text-blue-600 cursor-pointer transition-all duration-300 transform hover:scale-110"
+                />
+              </>
+            )}
           </div>
         </div>
       </section>
